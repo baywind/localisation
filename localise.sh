ddb4e70ef0f96043c243224354862a857e9faa73
Gennady Kushnir <genkush@rujel.net>
2010-01-21 22:30:27 +0300

    версия 0.8.2

--- localise.sh
+++ localise.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 WORKSPACE=/Users/gena/Documents/WOProj/_workspace/rujel
-LOCALISATION=/Users/Shared/RujelDeploy/localisation/Strings
+LOCALISATION=/Users/Shared/RujelDeploy/localisation
 
 cd $WORKSPACE
 for f in */Resources/*.plist ; do
 	NEWNAME=`echo $f | sed "s/\/Resources\//_/g"`
-	cp -f $f $LOCALISATION/$NEWNAME
+	cp -f $f $LOCALISATION/Strings/$NEWNAME
 	echo $NEWNAME
 done
 
 for f in */Resources/*.html ; do
 	NEWNAME=`echo $f | sed "s/\/Resources\//_/g"`
-	cp -f $f $LOCALISATION/$NEWNAME
+	cp -f $f $LOCALISATION/Strings/$NEWNAME
 	echo $NEWNAME
 done
+
+cp -rf $WORKSPACE/Supplemental/RujelReports $LOCALISATION
