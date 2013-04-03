5f36df173dab3706a047022c4d1a8e05fced141c
Gennady Kushnir <genkush@rujel.net>
2013-04-03 16:32:15 +0400

    Версия 0.9.6

--- localise.sh
+++ localise.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-WORKSPACE=/Users/gena/Documents/WOProj/_workspace/rujel
+DISTRO=/Users/Shared/RujelDeploy/rpms/rujel/RUJEL
 LOCALISATION=/Users/Shared/RujelDeploy/localisation
 
-cd $WORKSPACE
-for f in */Resources/*.plist ; do
-	NEWNAME=`echo $f | sed "s/\/Resources\//_/g"`
+cd $DISTRO/Frameworks
+for f in *.framework/Resources/*.plist ; do
+	NEWNAME=`echo $f | sed "s/\.framework\/Resources\//_/g"`
 	cp -f $f $LOCALISATION/Strings/$NEWNAME
 	echo $NEWNAME
 done
+rm -f $LOCALISATION/Strings/*Info.plist
 rm -f $LOCALISATION/Strings/*access.plist
 
-for f in */Resources/*.html ; do
-	NEWNAME=`echo $f | sed "s/\/Resources\//_/g"`
+for f in *.framework/Resources/*.html ; do
+	NEWNAME=`echo $f | sed "s/\.framework\/Resources\//_/g"`
 	cp -f $f $LOCALISATION/Strings/$NEWNAME
 	echo $NEWNAME
 done
 
-cp -rf $WORKSPACE/Supplemental/RujelReports $LOCALISATION
+cp -f $DISTRO/Rujel.woa/Contents/Resources/Strings.plist $LOCALISATION/Strings/Rujel_Strings.plist
+echo Rujel_Strings.plist
+cp -f $DISTRO/RujelDiary.woa/Contents/Resources/Strings.plist $LOCALISATION/Strings/RujelDiary_Strings.plist
+echo RujelDiary_Strings.plist
+
+
+cp -rf $DISTRO/Configuration/RujelReports $LOCALISATION
